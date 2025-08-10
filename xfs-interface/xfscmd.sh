1bf8279e43543be3f629c58e086fb0e3ff16394d
tsa1123 <abhijithkrishnanunnipzr@gmail.com>
2025-08-10 17:17:51 +0530

    First Commit

---
+++ xfs-interface/xfscmd.sh
@@ -0,0 +1,31 @@
+load --os ../spl/spl_progs/os_startup_stage13.xsm
+load --init ../expl/sample_progs/login.xsm
+load --shell ../expl/sample_progs/shell_v2.xsm
+load --int=4 ../spl/spl_progs/int/int4.xsm
+load --int=5 ../spl/spl_progs/int/int5.xsm
+load --int=6 ../spl/spl_progs/int/int6.xsm
+load --int=7 ../spl/spl_progs/int/int7.xsm
+load --int=8 ../spl/spl_progs/int/int8_test.xsm
+load --int=9 ../spl/spl_progs/int/int9_stage19.xsm
+load --int=10 ../spl/spl_progs/int/int10.xsm
+load --int=11 ../spl/spl_progs/int/int11.xsm
+load --int=12 ../spl/spl_progs/int/int12.xsm
+load --int=13 ../spl/spl_progs/int/int13.xsm
+load --int=14 ../spl/spl_progs/int/int14.xsm
+load --int=15 ../spl/spl_progs/int/int15.xsm
+load --int=16 ../spl/spl_progs/int/int16.xsm
+load --int=17 ../spl/spl_progs/int/int17.xsm
+load --int=timer ../spl/spl_progs/int/timer.xsm
+load --int=console ../spl/spl_progs/int/console_int.xsm
+load --int=disk ../spl/spl_progs/int/disk_int.xsm
+load --exhandler ../exhandler.xsm
+load --library ../expl/library.lib
+load --idle ../expl/sample_progs/idle.xsm
+load --module 0 ../spl/spl_progs/module/module_0.xsm
+load --module 1 ../spl/spl_progs/module/module_1.xsm
+load --module 2 ../spl/spl_progs/module/module_2.xsm
+load --module 3 ../spl/spl_progs/module/module_3.xsm
+load --module 4 ../spl/spl_progs/module/module_4.xsm
+load --module 5 ../spl/spl_progs/module/mod_5test.xsm
+load --module 6 ../spl/spl_progs/module/mod_6test.xsm
+load --module 7 ../spl/spl_progs/module/boot_module.xsm
